9f8470925f61ecdea087238bfd4231e8040af7d6
howard9199 <howardimportant@gmail.com>
2025-10-13 18:05:58 +0800

    feat: add google analytic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nycu-adsl.github.io',
   base: '/',
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    partytown({
+        config: {
+          forward: ["dataLayer.push"],
+        },
+    }),
+  ],
   outDir: './dist',
   markdown: {}
 });
